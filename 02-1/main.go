9655e520357385b5c7a6ecc6b6e2fd3c14f0db65
Joe Beda <joe.github@bedafamily.com>
2024-12-02 13:46:29 -0800

    Day 2
    Signed-off-by: Joe Beda <joe.github@bedafamily.com>

---
+++ 02-1/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"golang.org/x/exp/constraints"
+)
+
+func Abs[T constraints.Integer](x T) T {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	scan := bufio.NewScanner(f)
+	var tot int
+
+report:
+	for scan.Scan() {
+		line := scan.Text()
+
+		var report []int
+		ss := strings.Fields(line)
+		for _, s := range ss {
+			n, err := strconv.Atoi(s)
+			if err != nil {
+				log.Fatal(err)
+			}
+			report = append(report, n)
+		}
+
+		last := report[0]
+		increasing := false
+		if report[1] > last {
+			increasing = true
+		}
+
+		for i := 1; i < len(report); i++ {
+			curr := report[i]
+			if (increasing && curr < last) || (!increasing && curr > last) || curr == last {
+				continue report
+			}
+
+			diff := Abs(curr - last)
+			if diff > 3 {
+				continue report
+			}
+			last = curr
+		}
+		tot++
+	}
+
+	fmt.Println(tot)
+}
